848a67a4ef67e9b5367007edae3b8e2d70936bc3
Guojun Liu <liuguojun.pku@gmail.com>
2012-04-28 19:44:51 +0800

    add List ADT

---
+++ Other/DataStructure/List/list.hpp
@@ -0,0 +1,20 @@
+// Author: Guojun Liu
+// Date: 2012/04/28
+//
+#ifndef LIST_H_
+#define LIST_H_
+
+class Link;
+
+class List
+{
+ public:
+  List() : _pHead(0) {}
+  ~List();
+  void Add(int) {}
+
+}
+
+
+#endif  // LIST_H_
+
