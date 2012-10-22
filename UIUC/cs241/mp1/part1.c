3290a61ec014bab8b4e49980a89ebfa062ce28b0
Guojun Liu <liuguojun.pku@gmail.com>
2012-10-22 13:52:09 +0800

    add mp1

---
+++ UIUC/cs241/mp1/part1.c
@@ -0,0 +1,21 @@
+/** @file part1.c */
+
+/*
+ * Machine Problem #1
+ * CS 241
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mp1-functions.h"
+
+/**
+ * (Edit this function to print out the ten "Illinois" lines in mp1-functions.c in order.)
+ */
+int main()
+{
+
+	return 0;
+}
