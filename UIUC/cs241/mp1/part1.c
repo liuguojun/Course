ee8d759f15aae85fa9c676b8fa084a701ad82390
Guojun Liu <liuguojun.pku@gmail.com>
2012-10-22 14:28:14 +0800

    finish part1

--- UIUC/cs241/mp1/part1.c
+++ UIUC/cs241/mp1/part1.c
@@ -16,6 +16,38 @@
  */
 int main()
 {
+  first_step(81);
+
+  int num = 132;
+  second_step(&num);
+
+  int tmp = 8942;
+  int *value[] = {&tmp};
+  double_step(value);
+
+  strange_step(0);
+
+  char str[] = "123";
+  empty_step(str);
+
+  char s1[] = "uuuuuuuuuuuuuuu";
+  two_step((void *)s1, s1);
+
+  three_step(s1, s1+2, s1+4);
+
+  char s2[4];
+  s2[0] = 0;
+  s2[1] = s2[0];
+  s2[2] = s2[1] + 8;
+  s2[3] = s2[2] + 8;
+  step_step_step(s2, s2, s2);
+
+  char c  = 'A';
+  it_may_be_odd(&c, (int)c);
+
+  int d = 2049;
+  the_end((void *)(&d), (void *)(&d));
+
 
 	return 0;
 }
